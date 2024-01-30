8f083e7362f50600cd6bbe322b201b8c5e5eb34e
Yojibot (Joe) <55696714+Yojijuku5@users.noreply.github.com>
2024-01-30 02:09:57 +0000

    Update page.tsx
    First!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,43 @@ export default function Home() {
           <h1 className="my-4">Yojiweb</h1>
         </div>
         <div className="flex-auto h-10 mx-4">
-          <h1 className="text-3xl bg-blue-500">Date 1</h1>
+          <h1 className="text-3xl bg-blue-500">30th January 2024</h1>
         </div>
         <div className="mx-4">
           <p className="pb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a tristique sem, 
-            condimentum egestas augue. Vestibulum tincidunt dignissim lobortis. Aliquam erat volutpat. 
-            Nam arcu quam, semper commodo laoreet vel, aliquet eget tortor. Proin pellentesque efficitur nunc ac porta. 
-            Cras vulputate ac dolor vel gravida. Sed eget libero a sem tincidunt convallis. Duis sit amet dolor id arcu pretium finibus. 
-            Morbi non convallis neque. Phasellus accumsan feugiat risus.
+            Finally got around to deploying the website I've been thinking about making for the longest time. It says a few things about me that I would rather work on this instead of my University dissertation, 
+            despite my dissertation being about something I genuinly find interesting and linked to the industry that I want to eventually go into.
           </p>
           <p className="pb-4">
-            Cras vitae felis non est efficitur fermentum sit amet vitae nibh. 
-            Suspendisse porta erat et ex pretium, ut finibus orci feugiat. Sed pellentesque non urna vel convallis. 
-            Mauris sagittis mattis felis, in molestie erat commodo in. Morbi eu quam ac lorem bibendum sodales ac sit amet odio. 
-            Nullam varius leo et turpis iaculis, at iaculis ipsum dignissim. Proin sit amet massa nec tortor sodales dapibus. 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor, odio in mattis euismod, magna mi pretium metus, 
-            non mollis eros est nec urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-            Nullam magna lectus, maximus a nisi eu, auctor mollis lacus.
+            For now, the website is very bare bones, consisting of... just this page and text. This post is even written directly into the source code haha, but hopefully I will 
+            feel motivated enough to evolve this page into something more whole. High up on the to-do list is to perhaps find a way to be able to post without directly entering the source code. 
+            Some more UI aesthetic as well (colours etc. currently being temporary), and then maybe a comment section, even if noone visits because I don't plan on telling the world about this site just yet. 
+            A database would probably do wonders on both the storage and the efficiency of the website too.
+          </p>
+          <p className="pb-4">
+            I'm aware of other blogging platforms, however I really wanted to make my own website from scratch for many reasons, which I will list below:
+          </p>
+          <ol className="list-decimal mx-4 pb-4">
+            <li>It will give me much needed programming experience</li>
+            <li>I never wanted to create a blog attached to a third service, and making it myself from scratch gives me the utmost freedom to do as I wish.</li>
+            <li>I think it will be fun to start from scratch and watch this website evolve, and if I'm really serious about it, in a few years time this could become quite the personal achievement.</li>
+            <li>I like programming, I really do!</li>
+          </ol>
+          <p className="pb-4">
+            ...And looking at the preview after finishing the list I see that the left margin for the list doesn't line up with the other paragraphs, despite being within the same 'div' 
+            and class. It's seemingly illogical quirks like this that warrant the addition of the final point in the above list. Oh well, after a bit more tweaking I got it to work, evidently.
+          </p>
+          <p className="pb-4">
+            As I think about wrapping up this post, I realise that I won't be able to timestamp the post (time currently 01:59AM), since I don't have such functionality implemented yet. Oh well, I'm sure I'll 
+            get around to that soon, right? For now, this is the birth of Yojinet (temp name?) :)
           </p>
         </div>
+      </div>
+    </main>
+  )
+}
+
+/*
         <div className="flex-auto h-10 mx-4">
           <h1 className="text-3xl bg-blue-500">Date 2</h1>
         </div>
@@ -47,123 +64,4 @@ export default function Home() {
             Nullam magna lectus, maximus a nisi eu, auctor mollis lacus.
           </p>
         </div>
-      </div>
-    </main>
-  )
-}
-
-/*
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
 */
