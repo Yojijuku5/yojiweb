2ad6b883c75bd3c128060734015ff1c99fe573aa
Yojibot <yojibot@outlook.com>
2026-02-03 19:55:31 +0900

    rewrite proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+export const config = {
+    matcher: ["/((?!_next|favicon.ico).*)"],
+}
+
+export default function proxy(req: NextRequest) {
+    const url = req.nextUrl
+    let hostName = req.headers.get("host") || ""
+
+    const rootDomain = "yojiweb.com"
+
+    let subDomain = ""
+    if (hostName.endsWith(`.${rootDomain}`)) {
+        subDomain = hostName.replace(`.${rootDomain}`, "").split(".")[0]
+    }
+
+    if (!subDomain || subDomain === "www" || hostName === rootDomain) {
+        return NextResponse.rewrite(new URL(`/`, req.url))
+    }
+
+    return NextResponse.rewrite(new URL(`/app/${subDomain}`, req.url))
+}
+
+/*
 export function proxy(req: NextRequest) {
     const host = req.headers.get("host") || ""
 
@@ -11,3 +34,4 @@ export function proxy(req: NextRequest) {
 
     return NextResponse.next()
 }
+*/
