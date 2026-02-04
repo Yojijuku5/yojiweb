176ad8b901045e160bf34d4404233f3db8594b50
Yojibot <yojibot@outlook.com>
2026-02-04 17:53:34 +0900

    fix api fetch

--- src/app/api/tracks/route.ts
+++ src/app/api/tracks/route.ts
@@ -3,14 +3,14 @@ import path from 'path'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-	const jsonPath = path.join(process.cwd(), "https://yojiweb.com/data/tracks.json")
+	const jsonPath = path.join(process.cwd(), "/data/tracks.json")
 
 	const raw = fs.readFileSync(jsonPath, "utf-8")
 
 	const tracks = JSON.parse(raw)
 
 	const result = tracks.map((track: any) => ({
-		src: `/audio/${track.src}`,
+		src: `https://yojiweb.com/public/audio/${track.src}`,
 		title: track.title,
 		artist: track.artist,
 		bpm: track.bpm,
