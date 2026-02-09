b08eaf04298eefc283734a72407741c1c21b2ac2
Yojibot <yojibot@outlook.com>
2026-02-09 16:04:01 +0900

    improve ui, volume slider

--- src/app/tatsujinradio/layout.tsx
+++ src/app/tatsujinradio/layout.tsx
@@ -1,7 +1,5 @@
 import '@/app/globals.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { baseUrl } from '@/app/sitemap'
@@ -42,7 +40,7 @@ export default function radioLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en" className={cx("text-black bg-white dark:text-white dark:bg-black", GeistSans.variable, GeistMono.variable)}>
+        <html lang="en" className={cx("text-white bg-gray-700", "taikoFont")}>
             <body className="antialiased max-w-5xl mx-4 mt-8 lg:mx-auto">
                 <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
                     {children}
