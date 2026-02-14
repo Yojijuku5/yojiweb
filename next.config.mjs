81d768425676999b3f1187fcce20128dcde051c1
Yojibot <yojibot@outlook.com>
2026-02-14 14:16:05 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    module.exports = {
-        async headers() {
-            return [{
-                source: '/audio/(.*)'
-                headers: [{
-                    key: 'Cache-Control'
-                    value: 'public, max-age=31536000, immutable'
-                }]
+    async headers() {
+        return [{
+            source: '/audio/(.*)'
+            headers: [{
+                key: 'Cache-Control'
+                value: 'public, max-age=31536000, immutable'
             }]
-        }
+        }]
     }
 };
 
