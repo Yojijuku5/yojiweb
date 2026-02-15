78b629b657b5107929cd5a091e2f391e71db7ca2
Yojibot <yojibot@outlook.com>
2026-02-15 15:51:13 +0900

    update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-    matcher: ["/((?!_next|favicon.ico|.*\\..*).*)"],
+    matcher: ["/((?!api|_next|favicon.ico|.*\\..*).*)"],
 }
 
 export default function proxy(req: NextRequest) {
